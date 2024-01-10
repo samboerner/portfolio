d687f364686f41dd93e9859d7f15d99238ca38ec
Sam Boerner <samboerner3@gmail.com>
2024-01-09 22:26:56 -0600

    Worked on the Project Portfolio section of the website.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-var tabLinks = document.getElementsByClassName("tab-links");
-var tabContents = document.getElementsByClassName("tab-contents");
-
+/**
+ * This function switches between tabs in the 'About Me' section.
+ */
 function opentab(contentName, title){
+    /* Define variables */
+    var tabLinks = document.getElementsByClassName("tab-links");
+    var tabContents = document.getElementsByClassName("tab-contents");
+
+    /* Loop through tab-links to remove any 'active-link' from class names */
     for(link of tabLinks){
         link.classList.remove("active-link");
     }
+    /* Same thing but remove 'active-tab' from tab-contents' class names */
     for(content of tabContents){
         content.classList.remove("active-tab");
     }
+
+    /* Add 'active-link' and 'active-tab' to the class names of the active tab */
     document.getElementById(title).classList.add("active-link");
     document.getElementById(contentName).classList.add("active-tab");
 }
