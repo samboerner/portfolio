1a08123e297f9269a612af115b2966f243211a3d
Sam Boerner <samboerner3@gmail.com>
2024-01-18 23:56:05 -0600

    More work on Flask app, still nonfunctioning.

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 from flask import Flask, render_template, request
-import requests
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 
 app = Flask(__name__, static_folder="static")
 
-@app.route("/", methods=["GET","POST"])
+# Setting up Google Sheets API
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
+@app.route("/", methods=["GET", "POST"])
 def submit_form():
     if request.method == "POST":
+        # Extract data from HTML form
         name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('message')
 
-        api = "https://sheets.googleapis.com/v4/spreadsheets/1E2O5SFWq6ntAIbL59YzdnpAnf2JcYBccON8vmxIAbQ0/values/Sheet1?key=AIzaSyCZFnYLz__w-0P4lLKEA--6JvTf7awjOms"
+        # Authenticate with Google Sheets
+        gc = gspread.authorize(creds)
+        sheet = gc.open('Messages').sheet1
 
-        data = {
-            "values": [[name, email, message]]
-        }
+        #Write data to Google Sheet
+        sheet.append_row([name, email, message])
 
-        response = requests.post(api, json=data)
-
-        confirmation = ""
-        if response.status_code == 200:
-            confirmation = "Your message has been sent!"
-        else:
-            confirmation = "Error submitting form"
+        confirmation = "Your message has been sent!"
 
         return render_template("index.html", confirmation=confirmation)
     else:
