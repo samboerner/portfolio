de37e65e200730d09b2e9dd673eaec9dd9631f88
samboerner <samboerner3@gmail.com>
2024-01-23 22:08:07 -0600

    Got form submission w/ out page refresh working.

--- static/script.js
+++ static/script.js
@@ -32,15 +32,30 @@ function closemenu(){
     menu.style.right = "-200px";
 }
 
-msg = document.getElementById("msg")
-$(document).on('submit','#contact-form',function(e){
-    e.preventDefault();
+/**
+ *  The following code handles the contact form submission
+ *  posting to the flask app.
+ */
+var form = document.getElementById("contact-form");
+var msg = document.getElementById("msg");
+
+function submitForm(event) {
+    event.preventDefault();
     $.ajax({
-        type:'POST',
-        url:'/',
-        success:function()
-        {
-            alert('success');
+        type: 'POST',
+        url: '/',
+        data: $('form').serialize(),
+        success: function() {
+           $('#name').val('');
+           $('#email').val('');
+           $('#message').val('');
+           msg.innerHTML = "Your message has been sent!";
+            setTimeout(function(){
+                msg.innerHTML = ""
+            }, 5000);
+            form.reset();
         }
-    })
-});
+    });
+}
+
+form.addEventListener('submit', submitForm);
